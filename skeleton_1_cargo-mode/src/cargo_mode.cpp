3bb6eabc658c9c21d6565a2bb2df0274dfcf3a80
Ian Scholl <ian@auterion.com>
2024-10-10 16:01:40 +0200

    added TODO steps

--- skeleton_1_cargo-mode/src/cargo_mode.cpp
+++ skeleton_1_cargo-mode/src/cargo_mode.cpp
@@ -12,6 +12,10 @@ private:
     auterion::SystemState _system_state;
     auterion::LocalPositionAssessor _local_position_assessor;
 
+    // TODO #1: define your global variables here
+
+
+
 public:
     explicit MyMode(auterion::SDK& sdk, auterion::multicopter::GotoControlLimits& limits,
                     auterion::LocalPositionAssessorConfig& position_assessor_config)
@@ -23,12 +27,19 @@ public:
 
         _mode.onActivate([this]() {
             std::cout << "Cargo Mode activated. Hello!!!" << std::endl;
+
+            // TODO #2: Add your variables initializations here
+
+
         });
 
         _mode.onDeactivate([]() { std::cout << "Cargo Mode deactivated" << std::endl; });
 
         _mode.onUpdateSetpoint([this](float dt_s) -> auterion::Setpoint {
             (void)dt_s;
+
+            // TODO #4: Implement your cargo waypoint algorithm here
+
 
             auterion::multicopter::LocalFrameGotoSetpoint setpoint;
             return setpoint;
@@ -39,9 +50,15 @@ public:
 
 int main(int argc, char* argv[]) {
     auterion::SDK sdk(argc, argv, "cargo_example");
+
+    auto ret = rcutils_logging_set_logger_level(sdk.defaultRosHandle()->get_logger().get_name(), RCUTILS_LOG_SEVERITY_DEBUG);
     
     auterion::multicopter::GotoControlLimits goto_limits{};
     auterion::LocalPositionAssessorConfig position_assessor_config{};
+
+    // TODO #3: Define your speed limits and position assessor thresholds here (see example_1_goto-mode for inspiration)
+
+
 
     MyMode my_mode(sdk, goto_limits, position_assessor_config);
 
